41d6f3dedd519ba10d53bca5bdefb74efa8275c7
Jack White <jackwhitetheblackcat@gmail.com>
2022-08-09 20:55:45 -0700

    Create hungerchecker.py

---
+++ hungerchecker.py
@@ -0,0 +1,9 @@
+import datetime
+from datetime import date 
+today = date.today()
+
+def is_jack_hungry():
+    if today == today:
+            print("It is "+ str(today) + " and Jack is hungry.")
+
+is_jack_hungry()
